c903f38a6924c62608d1df79afcea23cc00ca8e3
djdiskmachine <110535302+djdiskmachine@users.noreply.github.com>
2025-12-15 19:00:40 +0000

    Yet another MacOS path fix go two levels up after adding the .app move garlicos/muos shell scripts up one level

--- projects/resources/packaging/lgpt_package.sh
+++ projects/resources/packaging/lgpt_package.sh
@@ -27,6 +27,7 @@ collect_resources() { #1PLATFORM #2lgpt.*-exe
   elif [ "$1" == "MACOS" ]; then # .app is a folder
     zip -9 $PACKAGE -j $CONTENTS
     zip -9yr $PACKAGE LittleGPTracker.app/
+    cd ../../ # Since we had to go here in the first place because MacOS globbing something something..
   else # all the others go in the bin
     mkdir bin ; cp $CONTENTS bin
     zip -9 $PACKAGE bin/* && rm -r bin/
