djdiskmachine/LittleGPTracker

commit eb5551598e334e6dcdc1fa42d75107af79046845

Don't allow creating a new project with the same name in the same place (#215)
* Don't allow creating a new project with the same name in the same place

Allow creating a project with the same name in a different path
Pass currentDirectory to NewProjectDialog
Path field added to NewProjectDialog
When randomising a project name, re run randomizer if name already in use

Adresses https://github.com/djdiskmachine/LittleGPTracker/issues/175

sources/Application/Views/ModalDialogs/NewProjectDialog.h
@@ -9,20 +9,22 @@
 
 class NewProjectDialog:public ModalView {
 public:
-	NewProjectDialog(View &view) ;
-	virtual ~NewProjectDialog() ;
+  NewProjectDialog(View &view, Path currentPath = "");
+  virtual ~NewProjectDialog();
 
-	virtual void DrawView() ;
-	virtual void OnPlayerUpdate(PlayerEventType ,unsigned int currentTick) ;
-	virtual void OnFocus() ;
-	virtual void ProcessButtonMask(unsigned short mask,bool pressed) ;
+  virtual void DrawView();
+  virtual void OnPlayerUpdate(PlayerEventType, unsigned int currentTick);
+  virtual void OnFocus();
+  virtual void ProcessButtonMask(unsigned short mask, bool pressed);
 
-	std::string GetName() ;
+  std::string GetName();
+
 private:
-	int selected_ ;
-	int lastChar_ ;
-	char name_[MAX_NAME_LENGTH+1] ;
-	int currentChar_ ;
+  Path currentPath_;
+  int selected_;
+  int lastChar_;
+  char name_[MAX_NAME_LENGTH + 1];
+  int currentChar_;
 } ;
 
 #endif

sources/Application/Model/Project.h
@@ -20,7 +20,7 @@
 
 #define PROJECT_NUMBER "1"
 #define PROJECT_RELEASE "6"
-#define BUILD_COUNT "0-bacon0"
+#define BUILD_COUNT "0-bacon1"
 
 #define MAX_TAP 3
 

sources/Application/Views/ModalDialogs/NewProjectDialog.cpp
@@ -2,18 +2,15 @@
 #include "NewProjectDialog.h"
 #include "Application/Utils/RandomNames.h"
 
-static char *buttonText[BUTTONS_LENGTH] = {
-	(char *)"Regen",
-	(char *)"Ok",
-	(char *)"Cancel"
-} ;
+static char *buttonText[BUTTONS_LENGTH] = {(char *)"Random", (char *)"Ok",
+                                           (char *)"Cancel"};
 
 #define DIALOG_WIDTH 20
 
-NewProjectDialog::NewProjectDialog(View &view):ModalView(view) {}
+NewProjectDialog::NewProjectDialog(View &view, Path currentPath)
+    : ModalView(view), currentPath_(currentPath) {}
 
-NewProjectDialog::~NewProjectDialog() {
-}
+NewProjectDialog::~NewProjectDialog() {}
 
 void NewProjectDialog::DrawView() {
 
@@ -49,10 +46,11 @@ void NewProjectDialog::DrawView() {
         props.invert_=(selected_==i+1) ;
 		DrawString(x,4,text,props) ;
     }
-};
+    View::EnableNotification();
+}
 
-void NewProjectDialog::OnPlayerUpdate(PlayerEventType ,unsigned int currentTick) {
-};
+void NewProjectDialog::OnPlayerUpdate(PlayerEventType,
+                                      unsigned int currentTick) {};
 
 void NewProjectDialog::OnFocus() {
 	selected_=currentChar_=0;
@@ -71,7 +69,7 @@ void NewProjectDialog::ProcessButtonMask(unsigned short mask,bool pressed) {
 	  // A modifier
       if (mask & EPBM_A) {
           if (mask == EPBM_A) {
-              std::string randomName = getRandomName();
+              std::string randomName = "";
               switch (selected_) {
               case 0:
                   if (name_[currentChar_] == ' ') {
@@ -80,14 +78,22 @@ void NewProjectDialog::ProcessButtonMask(unsigned short mask,bool pressed) {
                   isDirty_ = true;
                   break;
               case 1:
-                  std::fill(name_ + randomName.length(),
-                            name_ + sizeof(name_) / sizeof(name_[0]), ' ');
-                  strncpy(name_, randomName.c_str(), randomName.length());
-                  lastChar_ = currentChar_ = randomName.length() - 1;
+                  do {
+                      randomName = getRandomName();
+                      std::fill(name_ + randomName.length(),
+                      name_ + sizeof(name_) / sizeof(name_[0]), ' ');
+                      strncpy(name_, randomName.c_str(), randomName.length());
+                      lastChar_ = currentChar_ = randomName.length() - 1;
+                  } while (currentPath_.Descend(GetName()).Exists());
                   isDirty_ = true;
                   break;
               case 2:
-                  EndModal(1);
+                  if (currentPath_.Descend(GetName()).Exists()) {
+                      std::string res("Name " + std::string(name_) + " busy");
+                      View::SetNotification(res.c_str(), -6);
+                  } else {
+                      EndModal(1);
+                  }
                   break;
               case 3:
                   EndModal(0);

sources/Application/Views/ModalDialogs/SelectProjectDialog.cpp
@@ -9,11 +9,7 @@
 #define LIST_SIZE 20
 #define LIST_WIDTH 32
 
-static char *buttonText[3]= {
-	"Load",
-	"New",
-	"Exit"	
-} ;
+static char *buttonText[3] = {"Load", "New", "Exit"};
 
 Path SelectProjectDialog::lastFolder_("root:") ;
 int SelectProjectDialog::lastProject_ = 0 ;
@@ -25,12 +21,11 @@ static void NewProjectCallback(View &v,ModalView &dialog) {
 		std::string selected=npd.GetName() ;
 		SelectProjectDialog &spd=(SelectProjectDialog&)v ;
 		Result result = spd.OnNewProject(selected) ;
-    if (result.Failed())
-    {
-      Trace::Error(result.GetDescription().c_str());
+        if (result.Failed()) {
+            Trace::Error(result.GetDescription().c_str());
+        }
     }
-	} 
-} ;
+};
 
 SelectProjectDialog::SelectProjectDialog(View &view):ModalView(view),content_(true) {
 }
@@ -45,16 +40,17 @@ void SelectProjectDialog::DrawView() {
 	GUITextProperties props ;
 
 	SetColor(CD_NORMAL) ;
+    View::EnableNotification();
 
-// Draw projects
+    // Draw projects
 
-	int x=1 ;
-	int y=1 ;
+    int x = 1;
+    int y = 1;
 
-	if (currentProject_<topIndex_) {
-		topIndex_=currentProject_ ;
-	} ;
-	if (currentProject_>=topIndex_+LIST_SIZE) {
+    if (currentProject_ < topIndex_) {
+        topIndex_ = currentProject_;
+    };
+    if (currentProject_>=topIndex_+LIST_SIZE) {
 		topIndex_=currentProject_-LIST_SIZE+1 ;
 	} ;
 
@@ -113,12 +109,11 @@ void SelectProjectDialog::DrawView() {
 		x=offset*(i+1)-strlen(text)/2 ;
 		props.invert_=(i==selected_)?true:false ;
 		DrawString(x,y,text,props) ;
-	}	
-
+    }
 };
 
-void SelectProjectDialog::OnPlayerUpdate(PlayerEventType ,unsigned int currentTick) {
-};
+void SelectProjectDialog::OnPlayerUpdate(PlayerEventType,
+                                         unsigned int currentTick) {};
 
 void SelectProjectDialog::OnFocus() {
 
@@ -129,29 +124,30 @@ void SelectProjectDialog::OnFocus() {
 
 void SelectProjectDialog::ProcessButtonMask(unsigned short mask,bool pressed) {
 	if (!pressed) return ;
-	
-	if (mask&EPBM_B) {         
-		if (mask&EPBM_UP) warpToNextProject(-LIST_SIZE) ;
-		if (mask&EPBM_DOWN) warpToNextProject(LIST_SIZE) ;
-	} else {
 
-	  // A modifier
-	  if (mask&EPBM_A) { 
-		switch(selected_) {
+    if (mask&EPBM_B) {
+        if (mask & EPBM_UP)
+            warpToNextProject(-LIST_SIZE);
+        if (mask&EPBM_DOWN) warpToNextProject(LIST_SIZE) ;
+    } else {
+
+        // A modifier
+        if (mask & EPBM_A) {
+            switch (selected_) {
 			case 0: // load
 				{
-					//locate folder user had selected when they hit a
-					int count=0 ;
-					Path *current=0 ;
+                // locate folder user had selected when they hit a
+                int count = 0;
+                Path *current = 0;
 
-					IteratorPtr<Path> it(content_.GetIterator()) ;
-					for(it->Begin();!it->IsDone();it->Next()) {
-						if (count==currentProject_) {
-							current=&it->CurrentItem() ;
-							break ;
-						}
-						count++ ;
-					}
+                IteratorPtr<Path> it(content_.GetIterator());
+                for (it->Begin(); !it->IsDone(); it->Next()) {
+                    if (count == currentProject_) {
+                        current = &it->CurrentItem();
+                        break;
+                    }
+                    count++;
+                }
 
 					//check if folder is a project, indicated by 'lgpt' being the first 4 characters of the folder name
 					std::string name = current->GetName() ;
@@ -178,20 +174,21 @@ void SelectProjectDialog::ProcessButtonMask(unsigned short mask,bool pressed) {
 			}
 			case 1: // new
 			{
-				NewProjectDialog *npd=new NewProjectDialog(*this) ;
-				DoModal(npd,NewProjectCallback) ;
+                NewProjectDialog *npd =
+                    new NewProjectDialog(*this, currentPath_);
+                DoModal(npd,NewProjectCallback) ;
 				break ;
-			}
-			case 2: // Exit ;
-				EndModal(0) ;
+            }
+            case 2: // Exit ;
+                EndModal(0) ;
 				break ;
 		}
-	  } else {
+        } else {
 
-		  // R Modifier
+            // R Modifier
 
-          	if (mask&EPBM_R) {
-	    	} else {
+            if (mask & EPBM_R) {
+            } else {
                 // No modifier
 				if (mask==EPBM_UP) warpToNextProject(-1) ;
 				if (mask==EPBM_DOWN) warpToNextProject(1) ;
@@ -204,16 +201,16 @@ void SelectProjectDialog::ProcessButtonMask(unsigned short mask,bool pressed) {
 					selected_=(selected_+1)%3 ;
 					isDirty_=true ;
 				}
-		    }
-	  } 
-	}
+            }
+        }
+    }
 };
 
 void SelectProjectDialog::warpToNextProject(int amount) {
 
-	int offset=currentProject_-topIndex_ ;
-	int size=content_.Size() ;
-	currentProject_+=amount ;
+    int offset = currentProject_ - topIndex_;
+    int size = content_.Size();
+    currentProject_+=amount ;
 	if (currentProject_<0) currentProject_+=size ;
 	if (currentProject_>=size) currentProject_-=size ;
 
@@ -223,8 +220,7 @@ void SelectProjectDialog::warpToNextProject(int amount) {
 			topIndex_=0 ;
 		} ;
 	}
-	isDirty_=true ;
-
+    isDirty_ = true;
 }
 
 Path SelectProjectDialog::GetSelection() {
@@ -233,8 +229,14 @@ Path SelectProjectDialog::GetSelection() {
 
 Result SelectProjectDialog::OnNewProject(std::string &name) {
 
-	Path path = currentPath_.Descend(name);
-	Trace::Log("TMP","creating project at %s",path.GetPath().c_str());
+    Path path = currentPath_.Descend(name);
+    if (path.Exists()) {
+        Trace::Log("SelectProjectDialog:OnNewProj","path already exists %s", path.GetPath().c_str());
+		std::string res("Name " + name + " busy");
+		View::SetNotification(res.c_str(), 0);
+        return Result(res);
+    }
+    Trace::Log("TMP","creating project at %s",path.GetPath().c_str());
 	selection_ = path ;
 	Result result = FileSystem::GetInstance()->MakeDir(path.GetPath().c_str()) ;
 	RETURN_IF_FAILED(result, ("Failed to create project dir for '%s", path.GetPath().c_str()));
@@ -289,7 +291,5 @@ void SelectProjectDialog::setCurrentFolder(Path &path) {
 	//reset & redraw screen
 	topIndex_=0 ;
 	currentProject_=0 ;
-	isDirty_=true ;
+    isDirty_ = true;
 }
-
-

commit 407fe8ff56b61ee9e1e3f1eba40f16a928ef5704

Save As refactor (#217)
Save as now discards unsaved changes to current project
Unsaved changes only written to project under new name
Adds filesystem function Delete(Path&)
Adds optional argument to PersistencyService::Save(const char* name)


Save temp file
Copy temp file to new location
Delete temp file
Bump CHANGELOG

https://github.com/djdiskmachine/LittleGPTracker/issues/212

* Guard against forbidden paths 

Call ACTION_SAVE_AS NewProjectDialog with "root:" as path
Change default currentPath to "root:" (fix regression caused in #215)
Disallow accidental root folder access in FileSystem::Descend #219

sources/Application/Persistency/PersistencyService.cpp
@@ -7,12 +7,12 @@
 PersistencyService::PersistencyService():Service(MAKE_FOURCC('S','V','P','S')) {
 } ;
 
-void PersistencyService::Save() {
+void PersistencyService::Save(const char *name) {
 
-	Path filename("project:lgptsav.dat") ;
+    Path filename(name);
 
-	TiXmlDocument doc(filename.GetPath()) ;
-	TiXmlElement first("LITTLEGPTRACKER") ;
+    TiXmlDocument doc(filename.GetPath());
+    TiXmlElement first("LITTLEGPTRACKER") ;
 	TiXmlNode *node=doc.InsertEndChild(first) ;
 
 	// Loop on all registered service
@@ -25,7 +25,7 @@ void PersistencyService::Save() {
 	} ;
 
 	doc.SaveFile() ;
-} ;
+};
 
 bool PersistencyService::Load() {
 

sources/Application/Persistency/PersistencyService.h
@@ -8,8 +8,8 @@
 class PersistencyService: public Service,public T_Singleton<PersistencyService> {
 public:
 	PersistencyService() ;
-	void Save() ;
-	bool Load() ;
+    void Save(const char *name = "project:lgptsav.dat");
+    bool Load() ;
 } ;
 
 class PersistencyDocument: public TiXmlDocument {

sources/Application/Views/ModalDialogs/NewProjectDialog.h
@@ -9,7 +9,7 @@
 
 class NewProjectDialog:public ModalView {
 public:
-  NewProjectDialog(View &view, Path currentPath = "");
+  NewProjectDialog(View &view, Path currentPath = "root:");
   virtual ~NewProjectDialog();
 
   virtual void DrawView();

sources/Application/Views/ProjectView.cpp
@@ -38,8 +38,8 @@ static void SaveAsProjectCallback(View &v,ModalView &dialog) {
 		Path path_dstprjdir = Path(str_dstprjdir);
 		Path path_dstsmpdir = Path(str_dstsmpdir);
 
-		Path path_srclgptdatsav = path_srcprjdir.GetPath() + "lgptsav.dat";
-		Path path_dstlgptdatsav = path_dstprjdir.GetPath() + "/lgptsav.dat";
+        Path path_srclgptdatsav = path_srcprjdir.GetPath() + "lgptsav_tmp.dat";
+        Path path_dstlgptdatsav = path_dstprjdir.GetPath() + "/lgptsav.dat";
 
 		if (path_dstprjdir.Exists()) {
 			Trace::Log("ProjectView", "Dst Dir '%s' Exist == true",
@@ -55,10 +55,13 @@ static void SaveAsProjectCallback(View &v,ModalView &dialog) {
 			return;
 		};
 
-		FSS.Copy(path_srclgptdatsav,path_dstlgptdatsav);
+        if (FSS.Copy(path_srclgptdatsav, path_dstlgptdatsav) > -1) {
+            FSS.Delete(path_srclgptdatsav);
+        }
 
-		I_Dir *idir_srcsmpdir=FileSystem::GetInstance()->Open(path_srcsmpdir.GetPath().c_str());
-		if (idir_srcsmpdir) {
+        I_Dir *idir_srcsmpdir =
+            FileSystem::GetInstance()->Open(path_srcsmpdir.GetPath().c_str());
+        if (idir_srcsmpdir) {
 				idir_srcsmpdir->GetContent("*");
 				idir_srcsmpdir->Sort();
 				IteratorPtr<Path>it(idir_srcsmpdir->GetIterator());
@@ -270,8 +273,8 @@ void ProjectView::Update(Observable &,I_ObservableData *data) {
         }
         case ACTION_SAVE_AS: {
             PersistencyService *service = PersistencyService::GetInstance();
-            service->Save();
-            NewProjectDialog *mb = new NewProjectDialog(*this);
+            service->Save("project:lgptsav_tmp.dat");
+            NewProjectDialog *mb = new NewProjectDialog(*this, "root:");
             DoModal(mb, SaveAsProjectCallback);
             break;
         }

sources/System/FileSystem/FileSystem.cpp
@@ -76,9 +76,8 @@ std::string Path::GetCanonicalPath() {
 Path Path::Descend(const std::string& leaf)
 {
   std::string currentPath = GetPath();
-  if (currentPath[currentPath.size()-1] != '/')
-  {
-    currentPath += "/";
+  if (!currentPath.empty() && currentPath[currentPath.size() - 1] != '/') {
+      currentPath += "/";
   }
   return Path(currentPath+leaf);
 }
@@ -215,4 +214,20 @@ int FileSystemService::Copy(const Path &src,const Path &dst)
   isrc->Close();
   idst->Close();
   return nbwrite;
+}
+
+int FileSystemService::Delete(const Path &path) {
+    int result = -1;
+    std::string pathString = path.GetPath();
+    FileSystem * fs = FileSystem::GetInstance();
+
+    if (fs->GetFileType(pathString.c_str()) != FT_UNKNOWN) {
+		fs->Delete(pathString.c_str());
+        result += 1;
+		Trace::Log("FileSystemService"," Delete %s ", pathString.c_str());
+    } else {
+        Trace::Log("FS Delete","path does not exist: %s", pathString.c_str());
+    }
+
+    return result;
 }

sources/System/FileSystem/FileSystem.h
@@ -111,6 +111,7 @@ public:
 class FileSystemService {
  public:
   int Copy(const Path &src,const Path &dst);
+  int Delete(const Path &path);
 };
 
 #endif