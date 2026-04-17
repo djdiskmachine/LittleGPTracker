05b7a12c3730c48f6a2b7d77a6fd0ffbe022ec2c
djdiskmachine <110535302+djdiskmachine@users.noreply.github.com>
2026-04-17 16:21:15 +0000

    LD_PRELOAD instead of copy

--- projects/resources/GARLIC/LittleGPTracker.sh
+++ projects/resources/GARLIC/LittleGPTracker.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# Check if libav dependency is installed
-# Install it if not
-if [ ! -f /usr/lib/libavutil.so ]; then
-  cp lib/*so* /usr/lib/ &> lgpt.log
-fi
-
 progdir=$(dirname "$0")/lgpt
 cd $progdir
 HOME=$progdir
