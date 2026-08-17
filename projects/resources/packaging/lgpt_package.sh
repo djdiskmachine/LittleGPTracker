0150ddb715bac00ec6e18096cef2ef60104c168f
emartinez <enri.fc.98@gmail.com>
2026-08-17 19:39:42 +0200

    feat: added Switch for lgpt_package and also for the github actions

--- projects/resources/packaging/lgpt_package.sh
+++ projects/resources/packaging/lgpt_package.sh
@@ -27,15 +27,30 @@ collect_resources() { #1PLATFORM #2lgpt.*-exe
   elif [ "$1" == "MACOS" ]; then # .app is a folder
     zip -9 $PACKAGE -j $CONTENTS
     zip -9yr $PACKAGE LittleGPTracker.app/
+  elif [ "$1" == "SWITCH" ]; then # everything lives in one SD card folder
+    # hbmenu lists /switch/<dir>/<dir>.nro as a single entry, and the app
+    # hardcodes sdmc:/switch/lgpt for both bin: and root:, so the binary and
+    # its data - samplelib and the projects included - all go in that one
+    # folder. The user merges "switch" into their SD card root.
+    mkdir -p switch/lgpt ; cp $CONTENTS switch/lgpt
+    mv switch/lgpt/$2 switch/lgpt/lgpt.nro
+    cp -r ./resources/packaging/samplelib switch/lgpt
+    # -type d so the lgpt_* project folders come across as folders, and so it
+    # doesn't also pick up lgpt_package.sh sitting next to them
+    find ./resources/packaging -maxdepth 1 -type d -name "lgpt_*" -exec cp -r {} switch/lgpt \;
+    zip -9 -r $PACKAGE switch/ && rm -r switch/
   else # all the others go in the bin
     mkdir bin ; cp $CONTENTS bin
     zip -9 $PACKAGE bin/* && rm -r bin/
   fi
-  cd ./resources/packaging 
-  CONTENTS="../../../README.md ../../../CHANGELOG ../../../LICENSE"
-  CONTENTS+=" $(find . -name "samplelib" -type d)"
-  CONTENTS+=" $(find . -name "lgpt_*" -type d)"
-  zip -9 -r ../../$PACKAGE $CONTENTS
+  cd ./resources/packaging
+  # -j or these get stored as literal ../../../README.md, which unzip refuses
+  zip -9 -jq ../../$PACKAGE ../../../README.md ../../../CHANGELOG ../../../LICENSE
+  if [ "$1" != "SWITCH" ]; then # already staged inside switch/lgpt above
+    CONTENTS="$(find . -name "samplelib" -type d)"
+    CONTENTS+=" $(find . -name "lgpt_*" -type d)"
+    zip -9 -r ../../$PACKAGE $CONTENTS
+  fi
   CONTENTS="../../../docs/wiki/What-is-LittlePiggyTracker.md"
   CONTENTS+=" ../../../docs/wiki/config_xml.md"
   CONTENTS+=" ../../../docs/wiki/tips_and_tricks.md"
@@ -54,6 +69,7 @@ collect_resources CHIP lgpt.chip-exe
 collect_resources BITTBOY lgpt-bittboy.elf
 collect_resources GARLICPLUS lgpt-garlicplus.elf
 collect_resources RG35XXPLUS lgpt-rg35xxplus.elf
+collect_resources SWITCH lgpt-switch.nro
 collect_resources MACOS LittleGPTracker.app
 # collect_resources RS97 lgpt.dge
 # collect_resources STEAM lgpt.steam-exe
